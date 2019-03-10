dfae00d9daa42cc872794c679cb0695067342be4
Damian Stachowicz <damian93@gmail.com>
2019-03-10 15:39:29 +0100

    #2

---
+++ projects/lem/forms/src/lib/components/validator/validator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'lem-forms-validator',
+    templateUrl: './validator.component.html',
+    styleUrls: ['./validator.component.css']
+})
+export class ValidatorComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
--- projects/lem/forms/src/public_api.ts
+++ projects/lem/forms/src/public_api.ts
@@ -3,5 +3,6 @@
  */
 
 export * from './lib/components/input/input.component';
+export * from './lib/components/validator/validator.component';
 export * from './lib/forms.component';
 export * from './lib/forms.module';
